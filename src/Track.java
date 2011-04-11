01db33d9f5b2be9ebabc7406c46534052fd44b60
tforzaglia <tforzaglia.student@manhattan.edu>
2011-04-11 09:06:03 -0700

    New Track class with byte vector and boolean variable

--- src/Track.java
+++ src/Track.java
@@ -1,9 +1,36 @@
-import java.io.*;
+import java.util.Vector;
+
 public class Track {
 	
-	private String name;
-	private ByteArrayOutputStream audio;
+	//private String name;
+	//private ByteArrayOutputStream audio;
 	
+	private Vector<Byte> data = new Vector<Byte>();
+	private boolean enable = false;
+	
+	public Track( Vector<Byte> data, boolean enable)
+	{
+		this.data = data;
+		this.enable = false;
+		//volume
+	}
+	
+	public void setData(Vector<Byte> data)
+	{
+		this.data = data;
+	}
+	
+	public Vector<Byte> getData()
+	{
+		return this.data; 
+	}
+	
+	public void enableTrack()
+	{
+		this.enable = true;
+	}
+	
+	/*
 	public Track( String name, ByteArrayOutputStream audio )
 	{	
 		this.name = name;
@@ -14,6 +41,15 @@ public class Track {
 	{
 		return name;
 	}
+	*/
+	
+	
+	
+	
 	
 }
 
+//get set data 
+//get set volume
+//set enabled
+//take from each buffer and average 
