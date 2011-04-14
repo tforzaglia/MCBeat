d1efd2bbd88945fe1091580cf36ada2d7c210c8b
tforzaglia <tforzaglia.student@manhattan.edu>
2011-04-13 18:50:51 -0700

    now records into 1 of 4 buffers each time record is pressed

--- src/CaptureThread.java
+++ src/CaptureThread.java
@@ -6,7 +6,12 @@ import javax.sound.sampled.TargetDataLine;
 
 public class CaptureThread extends Thread {
 	byte tempBuffer[] = new byte[10000];
+	byte buffer1[] = new byte[10000];
+	byte buffer2[] = new byte[10000];
+	byte buffer3[] = new byte[10000];
+	byte buffer4[] = new byte[10000];
 	ByteArrayOutputStream byteArrayOutputStream;
+	
 	TargetDataLine targetDataLine;
 	Music music;
 
@@ -18,21 +23,80 @@ public class CaptureThread extends Thread {
 		
 		public void run()
 		{
-			byteArrayOutputStream = new ByteArrayOutputStream();
+			music.byteArrayOutputStream1 = new ByteArrayOutputStream();
+			music.byteArrayOutputStream2 = new ByteArrayOutputStream();
+			music.byteArrayOutputStream3 = new ByteArrayOutputStream();
+			music.byteArrayOutputStream4 = new ByteArrayOutputStream();
 			music.stopCapture = false;
+				
 			try{
-				//Loop until stopCapture is set by another thread 
-  				while( !music.stopCapture )
-  				{
-    				//Read data from the internal buffer of the data line.
-    				int count = targetDataLine.read( tempBuffer, 0, tempBuffer.length );
-    				if( count > 0 )
-    				{
-      					//Save data in output stream object.
-      					byteArrayOutputStream.write( tempBuffer, 0, count );
-    				}
-  				}
-			
+				if (music.numRecs == 1)
+				{
+					//Loop until stopCapture is set by another thread 
+	  				while( !music.stopCapture )
+	  				{
+	    				//Read data from the internal buffer of the data line.
+	    				int count = targetDataLine.read( buffer1, 0, buffer1.length );
+	    				if( count > 0 )
+	    				{
+	      					//Save data in output stream object.
+	      					music.byteArrayOutputStream1.write( buffer1, 0, count );
+	    				}
+	  				}
+	  				music.numRecs++;
+					music.byteArrayOutputStream1.close();
+				}
+				else if (music.numRecs == 2)
+				{
+					//Loop until stopCapture is set by another thread 
+	  				while( !music.stopCapture )
+	  				{
+	    				//Read data from the internal buffer of the data line.
+	    				int count = targetDataLine.read( buffer2, 0, buffer2.length );
+	    				if( count > 0 )
+	    				{
+	      					//Save data in output stream object.
+	      					music.byteArrayOutputStream2.write( buffer2, 0, count );
+	    				}
+	  				}
+	  				music.numRecs++;
+					music.byteArrayOutputStream2.close();
+				}
+				else if (music.numRecs == 3)
+				{
+					//Loop until stopCapture is set by another thread 
+	  				while( !music.stopCapture )
+	  				{
+	    				//Read data from the internal buffer of the data line.
+	    				int count = targetDataLine.read( buffer3, 0, buffer3.length );
+	    				if( count > 0 )
+	    				{
+	      					//Save data in output stream object.
+	      					music.byteArrayOutputStream3.write( buffer3, 0, count );
+	    				}
+	  				}
+	  				music.numRecs++;
+					music.byteArrayOutputStream3.close();
+				}
+				else if (music.numRecs == 4)
+				{
+					//Loop until stopCapture is set by another thread 
+	  				while( !music.stopCapture )
+	  				{
+	    				//Read data from the internal buffer of the data line.
+	    				int count = targetDataLine.read( buffer4, 0, buffer4.length );
+	    				if( count > 0 )
+	    				{
+	      					//Save data in output stream object.
+	      					music.byteArrayOutputStream4.write( buffer4, 0, count );
+	    				}
+	  				}
+	  				music.numRecs++;
+					music.byteArrayOutputStream4.close();
+				}
+				else
+					System.out.println("No more tracks available");
+  				/*
   				Scanner scan = new Scanner(System.in);
   				System.out.println("Name the track");
   				String name = scan.next();
@@ -40,16 +104,7 @@ public class CaptureThread extends Thread {
   				//add track name to string vector
   				music.TrackNames.add(name);
   				System.out.println(music.TrackNames);
-
-  				// add audio data to audio vector
-  				//Audio.add( byteArrayOutputStream );
-  				
-  				//Track t = new Track( name, byteArrayOutputStream );
-  				//Tracks.add( t );
-
-  				byteArrayOutputStream.close();
-			
-
+				*/
 			}catch (Exception e) {
   				System.out.println(e);
   				System.exit(0);
