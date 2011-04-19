d1d05e6c010fe627608dc6d8923ad7c48e6c60b6
Elvin Martinez <elvin210@Elvin-Martinezs-MacBook-Pro.local>
2011-04-19 15:21:21 -0400

    Minor tweaking to button layout. Added spacing between buttons.

--- src/Beat_Main.java
+++ src/Beat_Main.java
@@ -1,15 +1,13 @@
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Frame;
-import java.awt.Menu;
-import java.awt.MenuBar;
-import java.awt.MenuItem;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import javax.swing.BoxLayout;
 
+import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -45,7 +43,7 @@ public class Beat_Main implements WindowListener {
         content.setBackground(Color.white);
         content.setLayout(new FlowLayout());
         
-        // make 5 sets of of buttons
+        // 5 sets of of buttons
         JButton playButton1 = new JButton(new ImageIcon("sketchBlackPlay.png"));
         JButton stopRecButton1 = new JButton(new ImageIcon("sketchBlackStop.png"));
         JButton recordButton1 = new JButton(new ImageIcon("sketchBlackRecord.png"));
@@ -69,58 +67,93 @@ public class Beat_Main implements WindowListener {
         JButton recordButton4 = new JButton(new ImageIcon("sketchBlackRecord.png"));
         JButton pauseButton4 = new JButton(new ImageIcon("sketchBlackPause.png"));
         JButton playAllButton4 = new JButton(new ImageIcon("sketchBlackPush.png"));
-        
-//        JButton playButton = new JButton("play");
-//        JButton stopRecButton = new JButton("stop");
-//        JButton recordButton = new JButton("record");
-//        JButton pauseButton = new JButton("pause");
-//        JButton playAllButton = new JButton("playAll");
 
-//        playButton.addActionListener(new playButtonActionListener(recorder));
-//        stopRecButton.addActionListener(new stopRecButtonActionListener(recorder));
-//        recordButton.addActionListener(new recordButtonActionListener(recorder));
-//        pauseButton.addActionListener(new pauseButtonActionListener(recorder));
-//        playAllButton.addActionListener(new playAllButtonActionListener(recorder));
-
+        // these are the ActionListeners for our four sets of buttons
+//        playButton1.addActionListener(new playButtonActionListener(recorder));
+//        stopRecButton1.addActionListener(new stopRecButtonActionListener(recorder));
+//        recordButton1.addActionListener(new recordButtonActionListener(recorder));
+//        pauseButton1.addActionListener(new pauseButtonActionListener(recorder));
+//        playAllButton1.addActionListener(new playAllButtonActionListener(recorder));
+//        
+//        playButton2.addActionListener(new playButtonActionListener(recorder));
+//        stopRecButton2.addActionListener(new stopRecButtonActionListener(recorder));
+//        recordButton2.addActionListener(new recordButtonActionListener(recorder));
+//        pauseButton2.addActionListener(new pauseButtonActionListener(recorder));
+//        playAllButton2.addActionListener(new playAllButtonActionListener(recorder));
+//
+//        playButton3.addActionListener(new playButtonActionListener(recorder));
+//        stopRecButton3.addActionListener(new stopRecButtonActionListener(recorder));
+//        recordButton3.addActionListener(new recordButtonActionListener(recorder));
+//        pauseButton3.addActionListener(new pauseButtonActionListener(recorder));
+//        playAllButton3.addActionListener(new playAllButtonActionListener(recorder));
+//        
+//        playButton4.addActionListener(new playButtonActionListener(recorder));
+//        stopRecButton4.addActionListener(new stopRecButtonActionListener(recorder));
+//        recordButton4.addActionListener(new recordButtonActionListener(recorder));
+//        pauseButton4.addActionListener(new pauseButtonActionListener(recorder));
+//        playAllButton4.addActionListener(new playAllButtonActionListener(recorder));
+ 
         JPanel trackPanel1 = new JPanel();
         trackPanel1.setLayout(new BoxLayout(trackPanel1, 0));
+        trackPanel1.setBackground(Color.WHITE);
         trackPanel1.add(recordButton1);
+        trackPanel1.add(Box.createRigidArea(new Dimension(10,0))); // adds spacing between each button
         trackPanel1.add(stopRecButton1);
+        trackPanel1.add(Box.createRigidArea(new Dimension(10,0)));
         trackPanel1.add(pauseButton1);
+        trackPanel1.add(Box.createRigidArea(new Dimension(10,0)));
         trackPanel1.add(playButton1);
+        trackPanel1.add(Box.createRigidArea(new Dimension(10,0)));
         trackPanel1.add(playAllButton1);
         
         JPanel trackPanel2 = new JPanel();
         trackPanel2.setLayout(new BoxLayout(trackPanel2, 0));
+        trackPanel2.setBackground(Color.WHITE);
         trackPanel2.add(recordButton2);
+        trackPanel2.add(Box.createRigidArea(new Dimension(10,0)));
         trackPanel2.add(stopRecButton2);
+        trackPanel2.add(Box.createRigidArea(new Dimension(10,0)));
         trackPanel2.add(pauseButton2);
+        trackPanel2.add(Box.createRigidArea(new Dimension(10,0)));
         trackPanel2.add(playButton2);
+        trackPanel2.add(Box.createRigidArea(new Dimension(10,0)));
         trackPanel2.add(playAllButton2);
         
         JPanel trackPanel3 = new JPanel();
         trackPanel3.setLayout(new BoxLayout(trackPanel3, 0));
+        trackPanel3.setBackground(Color.WHITE);
         trackPanel3.add(recordButton3);
+        trackPanel3.add(Box.createRigidArea(new Dimension(10,0)));
         trackPanel3.add(stopRecButton3);
+        trackPanel3.add(Box.createRigidArea(new Dimension(10,0)));
         trackPanel3.add(pauseButton3);
+        trackPanel3.add(Box.createRigidArea(new Dimension(10,0)));
         trackPanel3.add(playButton3);
+        trackPanel3.add(Box.createRigidArea(new Dimension(10,0)));
         trackPanel3.add(playAllButton3);
         
         JPanel trackPanel4 = new JPanel();
         trackPanel4.setLayout(new BoxLayout(trackPanel4, 0));
+        trackPanel4.setBackground(Color.WHITE);
         trackPanel4.add(recordButton4);
+        trackPanel4.add(Box.createRigidArea(new Dimension(10,0)));
         trackPanel4.add(stopRecButton4);
+        trackPanel4.add(Box.createRigidArea(new Dimension(10,0)));
         trackPanel4.add(pauseButton4);
+        trackPanel4.add(Box.createRigidArea(new Dimension(10,0)));
         trackPanel4.add(playButton4);
+        trackPanel4.add(Box.createRigidArea(new Dimension(10,0)));
         trackPanel4.add(playAllButton4);
         
+        // lays out the 5 sets of buttons on to the container
         content.setLayout(new BoxLayout(content, 1));
         content.add(trackPanel1);
+        content.add(Box.createRigidArea(new Dimension(0,25))); // adds spacing between each container
         content.add(trackPanel2);
+        content.add(Box.createRigidArea(new Dimension(0,25)));
         content.add(trackPanel3);
+        content.add(Box.createRigidArea(new Dimension(0,25)));
         content.add(trackPanel4);
-
-
 
         f.pack();
         f.setVisible(true);
@@ -137,32 +170,26 @@ public class Beat_Main implements WindowListener {
     public void windowClosing(WindowEvent arg0) {
         System.exit(0);
     }
-
     @Override
     public void windowActivated(WindowEvent arg0) {
         // TODO Auto-generated method stub
     }
-
     @Override
     public void windowClosed(WindowEvent arg0) {
         // TODO Auto-generated method stub	
     }
-
     @Override
     public void windowDeactivated(WindowEvent arg0) {
         // TODO Auto-generated method stub	
     }
-
     @Override
     public void windowDeiconified(WindowEvent arg0) {
         // TODO Auto-generated method stub	
     }
-
     @Override
     public void windowIconified(WindowEvent arg0) {
         // TODO Auto-generated method stub	
     }
-
     @Override
     public void windowOpened(WindowEvent arg0) {
         // TODO Auto-generated method stub	
