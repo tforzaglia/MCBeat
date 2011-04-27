d8e824a03f9387fdb99f0d0cfe584a87e04b82c7
Brian Merriman <bmerriman.student@manhattan.edu>
2011-04-27 13:54:59 -0400

    Modified main()

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
+package mcbe;
 
 import java.awt.Canvas;
 import java.awt.Color;
@@ -55,110 +55,6 @@ public class Main {
         content.setBackground(Color.white);
         content.setLayout(new FlowLayout());
 
-        JButton playButton1 = new JButton(new ImageIcon("sketchBlackPlay.png"));
-        JButton stopRecButton1 = new JButton(new ImageIcon("sketchBlackStop.png"));
-        JButton recordButton1 = new JButton(new ImageIcon("sketchBlackRecord.png"));
-        JButton pauseButton1 = new JButton(new ImageIcon("sketchBlackPause.png"));
-        JButton pushButton1 = new JButton(new ImageIcon("sketchBlackPush.png"));
-        Canvas track1Display = new TrackDisplay(track1);
-
-        pauseButton1.addActionListener(new PauseButtonActionListener(track1));
-        playButton1.addActionListener(new PlayButtonActionListener(track1));
-        recordButton1.addActionListener(new RecordButtonActionListener(track1));
-        stopRecButton1.addActionListener(new StopRecButtonActionListener(track1));
-        pushButton1.addActionListener(new PushButtonActionListener(track1, trackMaster));
-
-        JPanel trackPanel1 = new JPanel();
-        trackPanel1.setLayout(new BoxLayout(trackPanel1, 0));
-
-        trackPanel1.add(recordButton1);
-        trackPanel1.add(stopRecButton1);
-        trackPanel1.add(Box.createRigidArea(new Dimension(10,0)));
-        trackPanel1.add(playButton1);
-        trackPanel1.add(pauseButton1);
-        trackPanel1.add(Box.createRigidArea(new Dimension(5,0)));
-        trackPanel1.add(track1);
-        trackPanel1.add(Box.createRigidArea(new Dimension(5,0)));
-        trackPanel1.add(pushButton1);
-
-
-        JButton playButton2 = new JButton(new ImageIcon("sketchBlackPlay.png"));
-        JButton stopRecButton2 = new JButton(new ImageIcon("sketchBlackStop.png"));
-        JButton recordButton2 = new JButton(new ImageIcon("sketchBlackRecord.png"));
-        JButton pauseButton2 = new JButton(new ImageIcon("sketchBlackPause.png"));
-        JButton pushButton2 = new JButton(new ImageIcon("sketchBlackPush.png"));
-        Canvas track2Display = new TrackDisplay(track2);
-
-        playButton2.addActionListener(new PlayButtonActionListener(track2));
-        pauseButton2.addActionListener(new PauseButtonActionListener(track2));
-        recordButton2.addActionListener(new RecordButtonActionListener(track2));
-        stopRecButton2.addActionListener(new StopRecButtonActionListener(track2));
-        pushButton2.addActionListener(new PushButtonActionListener(track2, trackMaster));
-
-        JPanel trackPanel2 = new JPanel();
-        trackPanel2.setLayout(new BoxLayout(trackPanel2, 0));
-        trackPanel2.add(recordButton2);
-        trackPanel2.add(stopRecButton2);
-        trackPanel2.add(Box.createRigidArea(new Dimension(10,0)));
-        trackPanel2.add(playButton2);
-        trackPanel2.add(pauseButton2);
-        trackPanel2.add(Box.createRigidArea(new Dimension(5,0)));
-        trackPanel2.add(track2Display);
-        trackPanel2.add(Box.createRigidArea(new Dimension(5,0)));
-        trackPanel2.add(pushButton2);
-
-        JButton playButton3 = new JButton(new ImageIcon("sketchBlackPlay.png"));
-        JButton stopRecButton3 = new JButton(new ImageIcon("sketchBlackStop.png"));
-        JButton recordButton3 = new JButton(new ImageIcon("sketchBlackRecord.png"));
-        JButton pauseButton3 = new JButton(new ImageIcon("sketchBlackPause.png"));
-        JButton pushButton3 = new JButton(new ImageIcon("sketchBlackPush.png"));
-        Canvas track3Display = new TrackDisplay(track3);
-
-
-
-        playButton3.addActionListener(new PlayButtonActionListener(track3));
-        pauseButton3.addActionListener(new PauseButtonActionListener(track3));
-        recordButton3.addActionListener(new RecordButtonActionListener(track3));
-        stopRecButton3.addActionListener(new StopRecButtonActionListener(track3));
-        pushButton3.addActionListener(new PushButtonActionListener(track3, trackMaster));
-
-        JPanel trackPanel3 = new JPanel();
-        trackPanel3.setLayout(new BoxLayout(trackPanel3, 0));
-        trackPanel3.add(recordButton3);
-        trackPanel3.add(stopRecButton3);
-        trackPanel3.add(Box.createRigidArea(new Dimension(10,0)));
-        trackPanel3.add(playButton3); 
-        trackPanel3.add(pauseButton3);
-        trackPanel3.add(Box.createRigidArea(new Dimension(5,0)));
-        trackPanel3.add(track3Display);
-        trackPanel3.add(Box.createRigidArea(new Dimension(5,0)));
-        trackPanel3.add(pushButton3);
-
-        JButton playButton4 = new JButton(new ImageIcon("sketchBlackPlay.png"));
-        JButton stopRecButton4 = new JButton(new ImageIcon("sketchBlackStop.png"));
-        JButton recordButton4 = new JButton(new ImageIcon("sketchBlackRecord.png"));
-        JButton pauseButton4 = new JButton(new ImageIcon("sketchBlackPause.png"));
-        JButton pushButton4 = new JButton(new ImageIcon("sketchBlackPush.png"));
-        Canvas track4Display = new TrackDisplay(track4);
-
-
-        playButton4.addActionListener(new PlayButtonActionListener(track4));
-        pauseButton4.addActionListener(new PauseButtonActionListener(track4));
-        recordButton4.addActionListener(new RecordButtonActionListener(track4));
-        stopRecButton4.addActionListener(new StopRecButtonActionListener(track4));
-        pushButton4.addActionListener(new PushButtonActionListener(track4, trackMaster));
-
-        JPanel trackPanel4 = new JPanel();
-        trackPanel4.setLayout(new BoxLayout(trackPanel4, 0));
-        trackPanel4.add(recordButton4);
-        trackPanel4.add(stopRecButton4);
-        trackPanel4.add(Box.createRigidArea(new Dimension(10,0)));
-        trackPanel4.add(playButton4);
-        trackPanel4.add(pauseButton4);
-        trackPanel4.add(Box.createRigidArea(new Dimension(5,0)));
-        trackPanel4.add(track4Display);
-        trackPanel4.add(Box.createRigidArea(new Dimension(5,0)));
-        trackPanel4.add(pushButton4);
 
         JButton playButtonMaster = new JButton(new ImageIcon("sketchBlackPlay.png"));
         JButton pauseButtonMaster = new JButton(new ImageIcon("sketchBlackPause.png"));
@@ -178,13 +74,13 @@ public class Main {
 
 
         content.setLayout(new BoxLayout(content, 1));
-        content.add(trackPanel1);
+        content.add(new TrackGUI(track1, trackMaster).getTrackPanel());
         content.add(Box.createRigidArea(new Dimension(0,10)));
-        content.add(trackPanel2);
+        content.add(new TrackGUI(track2, trackMaster).getTrackPanel());
         content.add(Box.createRigidArea(new Dimension(0,10)));
-        content.add(trackPanel3);
+        content.add(new TrackGUI(track3, trackMaster).getTrackPanel());
         content.add(Box.createRigidArea(new Dimension(0,10)));
-        content.add(trackPanel4);
+        content.add(new TrackGUI(track4, trackMaster).getTrackPanel());
         content.add(Box.createRigidArea(new Dimension(0,20)));
         content.add(trackPanelMaster);
 
