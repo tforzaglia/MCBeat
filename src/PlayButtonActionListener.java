22a477a30059b71cd73eddf266feeafc7ba7e68c
tforzaglia <tforzaglia.student@manhattan.edu>
2011-05-06 07:01:38 -0700

    now can either play from beginning or resume from paused spot

--- src/PlayButtonActionListener.java
+++ src/PlayButtonActionListener.java
@@ -1,15 +1,25 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.io.IOException;
 
 public class PlayButtonActionListener implements ActionListener {
 	Track track;
+        int count = 0;
 	public PlayButtonActionListener(Track track){
 		this.track = track;
 	}
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-                track.startPlay();
+                try{              
+                	if(count == 0){
+                		track.startPlay();//plays clip from beginning
+                		count++;//this will need to be changed when checks are added
+                	}
+                	else //if checked
+                		track.resume();//resumes a paused track
+                }catch(IOException ioe) {
+            		ioe.printStackTrace();
+                }
 	}
 
 }
