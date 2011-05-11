4cca4e974cddfd50f8d66301ec876225db107494
tforzaglia <tforzaglia.student@manhattan.edu>
2011-05-11 10:25:11 -0700

    play either clip or track based on loop checkbox

--- src/PlayButtonActionListener.java
+++ src/PlayButtonActionListener.java
@@ -4,19 +4,17 @@ import java.io.IOException;
 
 public class PlayButtonActionListener implements ActionListener {
 	Track track;
-        int count = 0;
 	public PlayButtonActionListener(Track track){
 		this.track = track;
 	}
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-                try{              
-                	if(count == 0){
-                		track.startPlay();//plays clip from beginning
-                		count++;//this will need to be changed when checks are added
-                	}
-                	else //if checked
-                		track.resume();//resumes a paused track
+                try{
+                	if(track.isLooping())
+                		track.resume();
+                	else
+                		track.startPlay();
+                		
                 }catch(IOException ioe) {
             		ioe.printStackTrace();
                 }
