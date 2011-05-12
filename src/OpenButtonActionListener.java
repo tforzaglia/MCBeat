2e5bc6f55997f17ec46ab6fcc897c6a78605561a
tforzaglia <tforzaglia.student@manhattan.edu>
2011-05-11 17:30:32 -0700

    added comments to the OpenButtonActionListener

--- src/OpenButtonActionListener.java
+++ src/OpenButtonActionListener.java
@@ -7,8 +7,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JFileChooser;
-//import mcbea.PushButtonActionListener;
-//import mcbea.Track;
+
+/*
+* action listener to open a .wav file which can be selected
+*  .wav file is loaded into the track's audio input stream
+*/
 
 public class OpenButtonActionListener implements ActionListener {
 
@@ -22,20 +25,19 @@ public class OpenButtonActionListener implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent arg0) {
-       
 
             int ret = fc.showDialog(null, "Open file");
 
             if (ret == JFileChooser.APPROVE_OPTION) {
             try {
+                //open the selected file
                 track.open(fc.getSelectedFile());
             } catch (IOException ex) {
                 Logger.getLogger(OpenButtonActionListener.class.getName()).log(Level.SEVERE, null, ex);
             }
                     System.out.println("OPENED");
-                    track.rename(fc.getSelectedFile().getName());
-                
-            }
-        
+                    //rename the track to the name of the selected file
+                    track.rename(fc.getSelectedFile().getName());                
+            }       
     }
 }
